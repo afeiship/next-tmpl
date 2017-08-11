004dfb1a65caf0a3dcdf01ef20e6b8ef76b985ab
afeiship <1290657123@qq.com>
2017-08-11 18:17:18 +0800

    issue: intial

---
+++ src/next-tmpl.js
@@ -0,0 +1,21 @@
+(function () {
+
+  var global = global || window || self || this;
+  var nx = global.nx || require('next-js-core2');
+  var formatRE = /(?:{)([\w.]+?)(?:})/gm;
+
+  var tmpl = nx.tmpl = function(inString,inArgs){
+    var result = inString || '';
+    var replaceFn = nx.isArray(inArgs) ? function (str, match) {
+        return inArgs[match];
+      } : function (str, match) {
+        return nx.path(inArgs, match);
+      };
+    return inString.replace(formatRE, replaceFn);
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = tmpl;
+  }
+
+}());
