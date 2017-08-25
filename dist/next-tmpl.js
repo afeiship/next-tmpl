8b17d52b3f7fe386636cada380df8aed4640f50d
afeiship <1290657123@qq.com>
2017-08-25 11:25:08 +0800

    issue: optmize

--- dist/next-tmpl.js
+++ dist/next-tmpl.js
@@ -4,7 +4,7 @@
   var nx = global.nx || require('next-js-core2');
   var FORMAT_RE = /(?:{)([\w.]+?)(?:})/gm;
 
-  var tmpl = nx.tmpl = function(inString,inArgs){
+  nx.tmpl = function(inString,inArgs){
     var result = inString || '';
     var replaceFn = nx.isArray(inArgs) ? function (str, match) {
         return inArgs[match];
@@ -15,7 +15,7 @@
   };
 
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports = tmpl;
+    module.exports = nx.tmpl;
   }
 
 }());
