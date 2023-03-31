437d0760a7d1d50b3e5bef3895d20fb3fed6fa74
afeiship <1290657123@qq.com>
2023-03-31 10:29:20 +0800

    fix: upt build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,4 @@ const task2 = new NxScripts({ name: 'qs', classify: false });
 
 [task1, task2].forEach(gulp.registry);
 
-gulp.task('default', gulp.series(['clean', 'nx:scripts.cjs']));
+gulp.task('default', gulp.series(['clean', 'nx:scripts']));
